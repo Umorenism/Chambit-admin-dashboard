c8ba337a82d69944fe1dc75eb01097111a0f92be
Umorenism <umorenismvic@gmail.com>
2024-08-21 05:26:22 -0700

    updated header

--- src/static/Header.tsx
+++ src/static/Header.tsx
@@ -1,10 +1,17 @@
-// src/components/Header.tsx
-import React from "react";
+import logo from "../asset/logo.png";
+import { CiSearch } from "react-icons/ci";
 
 const Header: React.FC = () => {
   return (
     <header className="fixed top-0 left-0 w-full h-16 bg-blue-500 text-white shadow-lg flex items-center px-4">
-      <h1 className="text-2xl font-bold">Admin Dashboard</h1>
+      <div className=" w-full flex justify-between items-center">
+        <img src={logo} alt="" className="h-10 " />
+        <div className="bg-slate-400 p-2 flex w-[50%] items-center rounded-md ">
+          <input type="text" className="w-full outline-none bg-transparent " />
+          <CiSearch />
+        </div>
+        <div></div>
+      </div>
     </header>
   );
 };
