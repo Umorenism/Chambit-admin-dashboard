fdf2c0dc11cca583afc514dfb30a92b6855536e9
Umorenism <umorenismvic@gmail.com>
2024-08-21 05:27:49 -0700

    updated header

--- src/static/Header.tsx
+++ src/static/Header.tsx
@@ -3,7 +3,7 @@ import { CiSearch } from "react-icons/ci";
 
 const Header: React.FC = () => {
   return (
-    <header className="fixed top-0 left-0 w-full h-16 bg-blue-500 text-white shadow-lg flex items-center px-4">
+    <header className="fixed top-0 left-0 w-full h-16 bg-gray-950 text-white shadow-lg flex items-center px-4">
       <div className=" w-full flex justify-between items-center">
         <img src={logo} alt="" className="h-10 " />
         <div className="bg-slate-400 p-2 flex w-[50%] items-center rounded-md ">
